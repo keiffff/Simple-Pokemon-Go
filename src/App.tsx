f81903d53c51b48be2c43ce742b8362074eeb19a
kei <tenguuenglish@gmail.com>
2019-07-29 07:26:19 +0900

    🔄Add Loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import {css, keyframes} from 'emotion';
+import {Button, Loader} from 'semantic-ui-react';
 import axios from 'axios';
 
 const indexPageStyle = css({
   textAlign: 'center',
   backgroundColor: '#282c34',
   minHeight: '100vh',
-  color: 'white',
 });
 
 const headerStyle = css({
@@ -16,6 +16,7 @@ const headerStyle = css({
   alignItems: 'center',
   justifyContent: 'center',
   fontSize: 'calc(10px + 2vmin)',
+  color: '#61DAFB',
 });
 
 const logoSpin = keyframes(`
@@ -32,6 +33,21 @@ const logoStyle = css({
   pointerEvents: 'none',
 });
 
+const wrapperStyle = css({
+  padding: '20px 0 20px 0',
+  width: '80%',
+  margin: '0 auto',
+  background: '#FFFFFF',
+  height: 'auto',
+  borderRadius: '10px',
+  marginBottom: '20px',
+  marginTop: '20px',
+});
+
+const pokemonShowStyle = css({
+  color: '#263238',
+});
+
 const DEFAULT_API_CONFIG = {
   baseURL: 'https://pokeapi.co/api/v2/',
   timeout: 5000,
@@ -40,15 +56,18 @@ const DEFAULT_API_CONFIG = {
 const axiosClient = axios.create(DEFAULT_API_CONFIG);
 
 const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [name, setName] = useState('');
   const [appearance, setAppeaRance] = useState('');
 
   const getPokemon = async (id: number) => {
     try {
+      setIsLoading(true);
       const res = await axiosClient.get(`/pokemon/${id}`);
       const pokemon = res.data;
       setName(pokemon.forms[0].name);
       setAppeaRance(pokemon.sprites.front_default);
+      setIsLoading(false);
 
       return pokemon;
     } catch (err) {
@@ -66,10 +85,21 @@ const App: React.FC = () => {
     <div className={indexPageStyle}>
       <header className={headerStyle}>
         <img src={logo} className={logoStyle} alt="logo" />
+        <p>Click button to show Pokemon randomly!</p>
       </header>
-      <img src={appearance} />
-      <p>{name}</p>
-      <button onClick={handleSubmit}>Show Pokemon (random)</button>
+      <div className={wrapperStyle}>
+        {isLoading ? (
+          <div className="ui active centered inline loader"></div>
+        ) : (
+          <div className={pokemonShowStyle}>
+            <img src={appearance} />
+            <p>{name}</p>
+          </div>
+        )}
+      </div>
+      <Button color="red" onClick={handleSubmit}>
+        Show Pokemon!
+      </Button>
     </div>
   );
 };
