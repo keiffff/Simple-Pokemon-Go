d2d6204257df605f7ded024e95c70a0dffae689f
kei <tenguuenglish@gmail.com>
2019-07-28 18:38:49 +0900

    👩‍🎤Introduce emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import {css, keyframes} from 'emotion';
+
+const indexPageStyle = css({
+  textAlign: 'center',
+  backgroundColor: '#282c34',
+  minHeight: '100vh',
+});
+
+const headerStyle = css({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontSize: 'calc(10px + 2vmin)',
+  color: 'white',
+});
+
+const logoSpin = keyframes(`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }`);
+
+const logoStyle = css({
+  animation: `${logoSpin} infinite 20s linear`,
+  height: '20vmin',
+  pointerEvents: 'none',
+});
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={indexPageStyle}>
+      <header className={headerStyle}>
+        <img src={logo} className={logoStyle} alt="logo" />
       </header>
     </div>
   );
-}
+};
 
 export default App;
